1fdf38635872b160e4afa752b8e2cf5325b30b41
Milan Petrovic <dev4press@users.noreply.github.com>
2017-07-20 13:45:39 +0200

    Update wp_bbp_subscribed_forums.sql

--- tables/wp_bbp_subscribed_forums.sql
+++ tables/wp_bbp_subscribed_forums.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `wp_bbp_subscribed_forums` (
   `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
   `forum_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'forum\'.',
-  INDEX `user_id` (`user_id` ASC),
-  INDEX `forum_id` (`forum_id` ASC),
-  UNIQUE INDEX `user_forum` (`user_id` ASC, `forum_id` ASC))
+  INDEX `user_id` (`user_id`),
+  INDEX `forum_id` (`forum_id`),
+  UNIQUE INDEX `user_forum` (`user_id`, `forum_id`))
 ENGINE = InnoDB;
