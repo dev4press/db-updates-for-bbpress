5f4ba53bda3c3f786de73890c8e725188b61fde6
Milan Petrovic <dev4press@users.noreply.github.com>
2017-07-20 16:09:38 +0200

    Create wp_bbp_topics.sql

---
+++ tables/wp_bbp_topics.sql
@@ -0,0 +1,21 @@
+/* bbp_topics
+   this table includes columns based on the meta_keys saved for each forum into the wp_postmeta,
+   and it has indexes on each ID based column.
+*/
+
+CREATE TABLE `wp_bbp_topics` (
+  `topic_id` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Connects to ID in the wp_posts table for the post_type = \'topic\'.',
+  `forum_id` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Connects to ID in the wp_posts table for the post_type = \'forum\'.',
+  `last_reply_id` BIGINT UNSIGNED NOT NULL DEFAULT 0,
+  `last_active_id` BIGINT UNSIGNED NOT NULL DEFAULT 0,
+  `reply_count` INT UNSIGNED NOT NULL DEFAULT 0,
+  `reply_count_hidden` INT UNSIGNED NOT NULL DEFAULT 0,
+  `total_reply_count` INT UNSIGNED NOT NULL DEFAULT 0,
+  `voice_count` INT UNSIGNED NOT NULL DEFAULT 1,
+  `last_active_time` DATETIME NULL DEFAULT '0000-00-00 00:00:00',
+  INDEX `topic_id` (`topic_id`),
+  INDEX `forum_id` (`forum_id`),
+  UNIQUE INDEX `topic_forum_id` (`topic_id`, `forum_id`),
+  INDEX `last_reply_id` (`last_reply_id`),
+  INDEX `last_active_id` (`last_active_id`))
+ENGINE = InnoDB;
