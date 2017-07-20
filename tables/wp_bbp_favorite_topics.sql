4fa5b83436e869051345a56f30cab8135a007db8
Milan Petrovic <dev4press@users.noreply.github.com>
2017-07-20 13:45:19 +0200

    Update wp_bbp_favorite_topics.sql

--- tables/wp_bbp_favorite_topics.sql
+++ tables/wp_bbp_favorite_topics.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `wp_bbp_favorite_topics` (
   `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
   `topic_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'topic\'.',
-  INDEX `user_id` (`user_id` ASC),
-  INDEX `topic_id` (`topic_id` ASC),
-  UNIQUE INDEX `user_topic` (`user_id` ASC, `topic_id` ASC))
+  INDEX `user_id` (`user_id`),
+  INDEX `topic_id` (`topic_id`),
+  UNIQUE INDEX `user_topic` (`user_id`, `topic_id`))
 ENGINE = InnoDB;
