06907894016d4ba2da7b7ef43d370e4afb0dc028
Milan Petrovic <dev4press@users.noreply.github.com>
2017-07-24 09:58:04 +0200

    Update wp_bbp_replies.sql

--- tables/wp_bbp_replies.sql
+++ tables/wp_bbp_replies.sql
@@ -1,3 +1,7 @@
+/* bbp_replies
+   this table connects replies to topics and forums. Unique index placed on the reply/topic/forum combination.
+*/
+
 CREATE TABLE `wp_bbp_replies` (
   `reply_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'reply\'.',
   `topic_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'topic\'.',
