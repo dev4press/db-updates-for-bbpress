efbd3b059c269f82f851e119b1c660d8bb2def3a
Milan Petrovic <dev4press@users.noreply.github.com>
2018-10-27 19:51:15 +0200

    Update wp_bbp_subscribed_forums.sql
    Primary ID column, and current timestamp datetime column with index.

--- tables/wp_bbp_subscribed_forums.sql
+++ tables/wp_bbp_subscribed_forums.sql
@@ -3,9 +3,13 @@
 */
 
 CREATE TABLE `wp_bbp_subscribed_forums` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
   `forum_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'forum\'.',
+  `subscribed` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
   INDEX `user_id` (`user_id`),
   INDEX `forum_id` (`forum_id`),
+  INDEX `subscribed` (`subscribed`),
   UNIQUE INDEX `user_forum` (`user_id`, `forum_id`))
 ENGINE = InnoDB;
