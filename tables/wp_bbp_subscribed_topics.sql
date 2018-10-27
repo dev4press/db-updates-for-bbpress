a6196bda6a126ee46ef057df0c770cc4a4c6e40d
Milan Petrovic <dev4press@users.noreply.github.com>
2018-10-27 23:37:39 +0200

    Update wp_bbp_subscribed_topics.sql

--- tables/wp_bbp_subscribed_topics.sql
+++ tables/wp_bbp_subscribed_topics.sql
@@ -3,11 +3,9 @@
 */
 
 CREATE TABLE `wp_bbp_subscribed_topics` (
-  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
   `topic_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'topic\'.',
   `subscribed` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
   INDEX `user_id` (`user_id`),
   INDEX `topic_id` (`topic_id`),
   INDEX `subscribed` (`subscribed`),
