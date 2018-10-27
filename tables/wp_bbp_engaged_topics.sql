e4aee822c92af1b2268172eae4501f71e7718a8f
Milan Petrovic <dev4press@users.noreply.github.com>
2018-10-27 21:27:44 +0200

    Update wp_bbp_engaged_topics.sql
    Changed default engaged column value.

--- tables/wp_bbp_engaged_topics.sql
+++ tables/wp_bbp_engaged_topics.sql
@@ -6,7 +6,7 @@ CREATE TABLE `wp_bbp_engaged_topics` (
   `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
   `topic_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'topic\'.',
-  `engaged` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `engaged` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   INDEX `user_id` (`user_id`),
   INDEX `topic_id` (`topic_id`),
