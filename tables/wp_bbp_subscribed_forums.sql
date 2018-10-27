26339b685c4a9e3550773863d27a3cbab4e71d73
Milan Petrovic <dev4press@users.noreply.github.com>
2018-10-27 23:36:11 +0200

    Update wp_bbp_subscribed_forums.sql
    Remived PK.

--- tables/wp_bbp_subscribed_forums.sql
+++ tables/wp_bbp_subscribed_forums.sql
@@ -3,11 +3,9 @@
 */
 
 CREATE TABLE `wp_bbp_subscribed_forums` (
-  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
   `forum_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'forum\'.',
   `subscribed` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
   INDEX `user_id` (`user_id`),
   INDEX `forum_id` (`forum_id`),
   INDEX `subscribed` (`subscribed`),
