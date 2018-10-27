65721f9f8802cb838e062e58fd72d206c5a503c5
Milan Petrovic <dev4press@users.noreply.github.com>
2018-10-27 21:27:02 +0200

    Create engaged_topics.sql
    Convert existing data into engagement topics data.

---
+++ convert/engaged_topics.sql
@@ -0,0 +1,15 @@
+INSERT INTO wp_bbp_engaged_topics (`user_id`, `topic_id`, `engaged`) (
+	SELECT 
+		posts.post_author AS user_id, 
+		postmeta.meta_value AS topic_id, 
+		posts.post_date AS engaged
+	FROM wp_posts AS posts
+	INNER JOIN wp_postmeta AS postmeta 
+		ON posts.ID = postmeta.post_id 
+		AND postmeta.meta_key = '_bbp_topic_id'
+	WHERE 
+		posts.post_type IN ('topic', 'reply') 
+		AND posts.post_status IN ('publish', 'closed')
+	GROUP BY postmeta.meta_value, posts.post_author
+	ORDER BY posts.post_date ASC
+);
