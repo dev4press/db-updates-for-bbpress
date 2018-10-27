f2649604ed5ee7cb08530c7416cfc0f5cdf93924
Milan Petrovic <dev4press@users.noreply.github.com>
2018-10-27 19:46:07 +0200

    Update wp_bbp_favorite_topics.sql
    Primary ID column, and current timestamp datetime column with index.

--- tables/wp_bbp_favorite_topics.sql
+++ tables/wp_bbp_favorite_topics.sql
@@ -3,9 +3,13 @@
 */
 
 CREATE TABLE `wp_bbp_favorite_topics` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
   `topic_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'topic\'.',
+  `favorited` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
   INDEX `user_id` (`user_id`),
   INDEX `topic_id` (`topic_id`),
+  INDEX `favorited` (`favorited`),
   UNIQUE INDEX `user_topic` (`user_id`, `topic_id`))
 ENGINE = InnoDB;
