51c7504dd32d0b8c3894f55462268b850e1500be
Milan Petrovic <dev4press@users.noreply.github.com>
2018-10-28 20:00:38 +0100

    Update wp_bbp_engaged_topics.sql
    Changed index order.

--- tables/wp_bbp_engaged_topics.sql
+++ tables/wp_bbp_engaged_topics.sql
@@ -6,8 +6,8 @@ CREATE TABLE `wp_bbp_engaged_topics` (
   `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
   `topic_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'topic\'.',
   `engaged` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+  UNIQUE INDEX `user_topic` (`user_id`, `topic_id`),
   INDEX `user_id` (`user_id`),
   INDEX `topic_id` (`topic_id`),
-  INDEX `engaged` (`engaged`),
-  UNIQUE INDEX `user_topic` (`user_id`, `topic_id`))
+  INDEX `engaged` (`engaged`))
 ENGINE = InnoDB;
