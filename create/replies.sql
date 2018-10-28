940869efbe5e0437fa3c5fff836c284266df1cd7
Milan Petrovic <dev4press@users.noreply.github.com>
2018-10-28 21:55:54 +0100

    Update replies.sql
    Wrong tables names.

--- create/replies.sql
+++ create/replies.sql
@@ -3,11 +3,11 @@ INSERT INTO wp_bbp_replies (`reply_id`, `topic_id`, `forum_id`) (
 	  p.ID, 
 	  CAST(t.meta_value AS UNSIGNED), 
 	  CAST(f.meta_value AS UNSIGNED)
-  FROM wp_3_posts p
-  INNER JOIN wp_3_postmeta t 
+  FROM wp_posts p
+  INNER JOIN wp_postmeta t 
 	  ON p.ID = t.post_id 
 	  AND t.meta_key = '_bbp_topic_id'
-  INNER JOIN wp_3_postmeta f 
+  INNER JOIN wp_postmeta f 
 	  ON p.ID = f.post_id 
 	  AND f.meta_key = '_bbp_forum_id'
   WHERE 
