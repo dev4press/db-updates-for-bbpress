5bd220ae259e3c24099b8aff19f8534fc6f29657
Milan Petrovic <dev4press@users.noreply.github.com>
2018-10-28 20:02:36 +0100

    Update wp_bbp_favorited_topics.sql
    Changed index order.

--- tables/wp_bbp_favorited_topics.sql
+++ tables/wp_bbp_favorited_topics.sql
@@ -6,8 +6,8 @@ CREATE TABLE `wp_bbp_favorited_topics` (
   `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
   `topic_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'topic\'.',
   `favorited` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE INDEX `user_topic` (`user_id`, `topic_id`),
   INDEX `user_id` (`user_id`),
   INDEX `topic_id` (`topic_id`),
-  INDEX `favorited` (`favorited`),
-  UNIQUE INDEX `user_topic` (`user_id`, `topic_id`))
+  INDEX `favorited` (`favorited`))
 ENGINE = InnoDB;
