f868a1d5f0f3ec96b4b622b32ac8386e115a8a9e
Milan Petrovic <dev4press@users.noreply.github.com>
2018-10-28 20:05:39 +0100

    Update wp_bbp_users.sql
    Few minor issues.

--- tables/wp_bbp_users.sql
+++ tables/wp_bbp_users.sql
@@ -3,10 +3,10 @@
    and it has indexes on each ID based column.
 */
 
-CREATE TABLE `wp_bbp_topics` (
+CREATE TABLE `wp_bbp_users` (
   `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
   `last_posted` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `last_active` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00`,
+  `last_active` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
   `last_topic_id` BIGINT UNSIGNED NOT NULL DEFAULT 0,
   `last_reply_id` BIGINT UNSIGNED NOT NULL DEFAULT 0,
   `topic_count` INT UNSIGNED NOT NULL DEFAULT 0,
