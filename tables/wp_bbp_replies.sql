8befdefaa3c8c3e9913303d17d017c37602ad952
Milan Petrovic <dev4press@users.noreply.github.com>
2018-10-28 20:54:33 +0100

    Update wp_bbp_replies.sql
    Minor fix.

--- tables/wp_bbp_replies.sql
+++ tables/wp_bbp_replies.sql
@@ -8,6 +8,6 @@ CREATE TABLE `wp_bbp_replies` (
   `forum_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'forum\'.',
   UNIQUE INDEX `reply_topic_forum` (`reply_id`, `topic_id`, `forum_id`),
   INDEX `reply_id` (`reply_id`),
-  INDEX `topic_id` (`topic_id),
+  INDEX `topic_id` (`topic_id`),
   INDEX `forum_id` (`forum_id`))
 ENGINE = InnoDB;
