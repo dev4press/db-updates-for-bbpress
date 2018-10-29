14694d442f11f93418ca5e57ab27ff607a3300aa
Milan Petrovic <dev4press@users.noreply.github.com>
2018-10-29 12:47:10 +0100

    Update wp_bbp_topics.sql
    Sticky column and more indexes.

--- tables/wp_bbp_topics.sql
+++ tables/wp_bbp_topics.sql
@@ -6,6 +6,7 @@
 CREATE TABLE `wp_bbp_topics` (
   `topic_id` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Connects to ID in the wp_posts table for the post_type = \'topic\'.',
   `forum_id` BIGINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Connects to ID in the wp_posts table for the post_type = \'forum\'.',
+  `sticky` VARCHAR(16) NOT NULL DEFAULT '' COMMENT 'Values can be: empty, sticky or front.',
   `last_reply_id` BIGINT UNSIGNED NOT NULL DEFAULT 0,
   `last_active_id` BIGINT UNSIGNED NOT NULL DEFAULT 0,
   `reply_count` INT UNSIGNED NOT NULL DEFAULT 0,
@@ -14,8 +15,10 @@ CREATE TABLE `wp_bbp_topics` (
   `voice_count` INT UNSIGNED NOT NULL DEFAULT 1,
   `last_active_time` DATETIME NULL DEFAULT '0000-00-00 00:00:00',
   UNIQUE INDEX `topic_forum_id` (`topic_id`, `forum_id`),
+  INDEX `sticky` (`sticky`),
   INDEX `topic_id` (`topic_id`),
   INDEX `forum_id` (`forum_id`),
   INDEX `last_reply_id` (`last_reply_id`),
-  INDEX `last_active_id` (`last_active_id`))
+  INDEX `last_active_id` (`last_active_id`),
+  INDEX `last_active_time` (`last_active_time`))
 ENGINE = InnoDB;
