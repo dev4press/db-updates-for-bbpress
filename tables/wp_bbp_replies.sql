2629b88efd800e8ad9e6d46d35eac0482de3e675
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-05 20:11:14 +0200

    Update wp_bbp_replies.sql
    Parent ID column added for threaded replies.

--- tables/wp_bbp_replies.sql
+++ tables/wp_bbp_replies.sql
@@ -6,6 +6,7 @@ CREATE TABLE `wp_bbp_replies` (
   `reply_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'reply\'.',
   `topic_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'topic\'.',
   `forum_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'forum\'.',
+  `parent_id` BIGINT UNSIGNED NOT NULL COMMENT 'To be used for threaded replies'
   UNIQUE INDEX `reply_topic_forum` (`reply_id`, `topic_id`, `forum_id`),
   INDEX `reply_id` (`reply_id`),
   INDEX `topic_id` (`topic_id`),
