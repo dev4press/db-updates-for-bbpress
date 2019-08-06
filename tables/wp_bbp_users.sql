8cd85833335810e338ac1854e3ac7bbc5d5322f2
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-06 10:03:01 +0200

    Update wp_bbp_users.sql
    Removed some columns.

--- tables/wp_bbp_users.sql
+++ tables/wp_bbp_users.sql
@@ -7,14 +7,10 @@ CREATE TABLE `wp_bbp_users` (
   `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
   `last_posted` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
   `last_active` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `last_topic_id` BIGINT UNSIGNED NOT NULL DEFAULT 0,
-  `last_reply_id` BIGINT UNSIGNED NOT NULL DEFAULT 0,
   `topic_count` INT UNSIGNED NOT NULL DEFAULT 0,
   `reply_count` INT UNSIGNED NOT NULL DEFAULT 0,
   UNIQUE INDEX `user_id` (`user_id`),
   INDEX `last_posted` (`last_posted`),
-  INDEX `last_active` (`last_active`),
-  INDEX `last_topic_id` (`last_topic_id`),
-  INDEX `last_reply_id` (`last_reply_id`)
+  INDEX `last_active` (`last_active`)
 )
 ENGINE = InnoDB;
