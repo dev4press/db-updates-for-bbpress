be70c3d70b5d246fe98d7115adbf5b55c2db4ebc
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-06 12:45:03 +0200

    Update wp_bbp_subscribed_topics.sql

--- tables/wp_bbp_subscribed_topics.sql
+++ tables/wp_bbp_subscribed_topics.sql
@@ -7,5 +7,5 @@ CREATE TABLE `wp_bbp_subscribed_topics` (
   `topic_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_posts table for the post_type = \'topic\'.',
   UNIQUE INDEX `user_topic` (`user_id`, `topic_id`),
   INDEX `user_id` (`user_id`),
-  INDEX `topic_id` (`topic_id`)))
+  INDEX `topic_id` (`topic_id`))
 ENGINE = InnoDB;
