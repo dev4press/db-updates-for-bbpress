e86ec03dc8bbb3777f77dce414d59946c9b6ae07
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-06 13:11:54 +0200

    Update wp_bbp_subscribed_forums.sql

--- create/wp_bbp_subscribed_forums.sql
+++ create/wp_bbp_subscribed_forums.sql
@@ -1,7 +1,8 @@
-INNER JOIN wp_bbp_subscribed_forums
+INSERT INTO wp_bbp_subscribed_forums
 SELECT 
    CAST(pm.meta_value AS UNSIGNED) AS user_id, 
-   pm.post_id AS topic_id
+   pm.post_id AS forum_id
 FROM wp_postmeta pm
-INNER JOIN wp_posts p ON p.ID = pm.post_id AND p.post_type = 'forum'
-WHERE meta_key = '_bbp_subscription'
+INNER JOIN wp_posts p ON p.ID = pm.post_id 
+AND p.post_type = 'forum'
+WHERE pm.meta_key = '_bbp_subscription'
