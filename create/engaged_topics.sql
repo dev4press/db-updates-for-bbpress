1f5b179ec96d5554cc77e63eeaef87d5f8528d3e
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-06 12:22:52 +0200

    Update engaged_topics.sql
    The updated query for updated table schema.

--- create/engaged_topics.sql
+++ create/engaged_topics.sql
@@ -1,15 +1,11 @@
-INSERT INTO wp_bbp_engaged_topics (`user_id`, `topic_id`, `engaged`) (
-	SELECT 
-		posts.post_author, 
-		postmeta.meta_value, 
-		posts.post_date
-	FROM wp_posts AS posts
-	INNER JOIN wp_postmeta AS postmeta 
-		ON posts.ID = postmeta.post_id 
-		AND postmeta.meta_key = '_bbp_topic_id'
-	WHERE 
-		posts.post_type IN ('topic', 'reply') 
-		AND posts.post_status IN ('publish', 'closed')
-	GROUP BY postmeta.meta_value, posts.post_author
-	ORDER BY posts.post_date ASC
-);
+INSERT INTO wp_bbp_engaged_topics
+SELECT 
+   DISTINCT 
+   posts.post_author AS user_id, 
+   CAST(postmeta.meta_value AS UNSIGNED) AS topic_ic
+FROM wp_posts AS posts
+INNER JOIN wp_postmeta AS postmeta ON posts.ID = postmeta.post_id AND postmeta.meta_key = '_bbp_topic_id'
+WHERE 
+   posts.post_type IN ('topic', 'reply') AND 
+   posts.post_status IN ('publish', 'closed')
+ORDER BY CAST(postmeta.meta_value AS UNSIGNED) ASC
