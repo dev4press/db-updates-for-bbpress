9a0094be077014a0c4cddef6871cb3c8df54f35e
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-08 11:41:25 +0200

    Update wp_bbp_replies.sql

--- generate/wp_bbp_replies.sql
+++ generate/wp_bbp_replies.sql
@@ -3,8 +3,8 @@ SELECT
     r.ID AS reply_id,
     t.ID AS topic_id,
     f.ID AS forum_id,
-    if (r.post_parent = t.ID, 0, t.post_parent) AS parent_id
+    0 AS parent_id
 FROM wp_posts r 
 INNER JOIN wp_posts t ON t.ID = r.post_parent AND t.post_type = 'topic'
 INNER JOIN wp_posts f ON f.ID = t.post_parent AND f.post_type = 'forum'
-WHERE r.post_type = 'reply'
+WHERE r.post_type = 'reply';
