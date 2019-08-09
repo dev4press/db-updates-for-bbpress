4b169ac1e5954e55404fe0120e3dfea6d6198195
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-09 17:55:58 +0200

    Update wp_bbp_replies.sql

--- generate/wp_bbp_replies.sql
+++ generate/wp_bbp_replies.sql
@@ -9,10 +9,3 @@ FROM wp_posts r
 INNER JOIN wp_posts t ON t.ID = r.post_parent AND t.post_type = 'topic'
 INNER JOIN wp_posts f ON f.ID = t.post_parent AND f.post_type = 'forum'
 WHERE r.post_type = 'reply' AND r.post_status IN ('publish', 'spam', 'trash', 'pending');
-
-/* Update parent ID's */
-UPDATE wp_bbp_replies r 
-INNER JOIN wp_posts p ON p.ID = r.reply_id AND p.post_type = 'reply'
-INNER JOIN wp_postmeta m ON p.ID = m.post_id AND m.meta_key = '_bbp_reply_to'
-SET r.parent_id = CAST(m.meta_value AS UNSIGNED)
-WHERE r.topic_id != CAST(m.meta_value AS UNSIGNED) AND CAST(m.meta_value AS UNSIGNED) > 0;
