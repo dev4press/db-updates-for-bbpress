faa165e7d3ed40c2c5ec350abbc34ddfe72c1566
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-09 10:17:02 +0200

    Update wp_bbp_forums.sql

--- generate/wp_bbp_forums.sql
+++ generate/wp_bbp_forums.sql
@@ -9,3 +9,10 @@ LEFT JOIN wp_postmeta ms ON ms.post_id = f.ID AND ms.meta_key = '_bbp_status'
 LEFT JOIN wp_postmeta mt ON mt.post_id = f.ID AND mt.meta_key = '_bbp_forum_type'
 WHERE f.post_type = 'forum' AND f.post_status IN ('publish', 'private', 'hidden'));
 
+/* Updated subforums counts */
+UPDATE wp_bbp_forums o LEFT JOIN (
+    SELECT f.forum_id, count(p.ID) AS subforums
+    FROM wp_bbp_forums f INNER JOIN wp_posts p ON f.forum_id = p.post_parent AND p.post_type = 'forum'
+    WHERE p.post_status IN ('publish', 'private', 'hidden')
+    GROUP BY f.forum_id) r ON o.forum_id = r.forum_id
+SET o.subforum_count = r.subforums;
