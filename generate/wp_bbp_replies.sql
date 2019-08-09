218f55a49cb34cf63e223f97a3655a2fb8895bf3
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-09 09:45:30 +0200

    Update wp_bbp_replies.sql

--- generate/wp_bbp_replies.sql
+++ generate/wp_bbp_replies.sql
@@ -8,7 +8,7 @@ SELECT
 FROM wp_posts r 
 INNER JOIN wp_posts t ON t.ID = r.post_parent AND t.post_type = 'topic'
 INNER JOIN wp_posts f ON f.ID = t.post_parent AND f.post_type = 'forum'
-WHERE r.post_type = 'reply';
+WHERE r.post_type = 'reply' AND r.post_status IN ('publish', 'spam', 'trash', 'pending');
 
 /* Update parent ID's */
 UPDATE wp_bbp_replies r 
