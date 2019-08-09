c66eea58acbb1116a6d1e279aac8b55e4746956b
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-09 15:52:43 +0200

    Create wp_bbp_engaged_topics.sql

---
+++ generate/wp_bbp_engaged_topics.sql
@@ -0,0 +1,8 @@
+INSERT INTO wp_bbp_engaged_topics
+        SELECT p.post_author, t.topic_id 
+        FROM wp_bbp_topics t 
+        INNER JOIN wp_posts p ON p.ID = t.topic_id AND p.post_status IN ('publish', 'closed')
+    UNION
+        SELECT p.post_author, r.topic_id 
+        FROM wp_bbp_replies r 
+        INNER JOIN wp_posts p ON p.ID = r.reply_id AND p.post_status = 'publish';
