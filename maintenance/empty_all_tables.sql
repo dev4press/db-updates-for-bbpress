34698452fbf72c5b8416a40de13105e4eb528a8d
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-09 17:48:37 +0200

    Create empty_all_tables.sql

---
+++ maintenance/empty_all_tables.sql
@@ -0,0 +1,8 @@
+TRUNCATE `wp_bbp_engaged_topics`;
+TRUNCATE `wp_bbp_favorited_topics`;
+TRUNCATE `wp_bbp_forums`;
+TRUNCATE `wp_bbp_replies`;
+TRUNCATE `wp_bbp_subscribed_forums`;
+TRUNCATE `wp_bbp_subscribed_topics`;
+TRUNCATE `wp_bbp_topics`;
+TRUNCATE `wp_bbp_users`;
