cce0aa9da173ac7aecbea689ef6d8ce0daa640b9
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-09 17:57:17 +0200

    Update wp_bbp_forums.sql

--- generate/wp_bbp_forums.sql
+++ generate/wp_bbp_forums.sql
@@ -1,14 +1,3 @@
-/* Insert initial forum ID's */
-INSERT INTO wp_bbp_forums (`forum_id`, `forum_status`, `forum_type`) ( 
-SELECT
-    f.ID AS forum_id,
-    IF (ms.meta_value IS NULL OR ms.meta_value = 'open', 'open', 'closed') AS forum_status,
-    IF (mt.meta_value IS NULL OR mt.meta_value = 'forum', 'forum', 'category') AS forum_type
-FROM wp_posts f 
-LEFT JOIN wp_postmeta ms ON ms.post_id = f.ID AND ms.meta_key = '_bbp_status'
-LEFT JOIN wp_postmeta mt ON mt.post_id = f.ID AND mt.meta_key = '_bbp_forum_type'
-WHERE f.post_type = 'forum' AND f.post_status IN ('publish', 'private', 'hidden'));
-
 /* Updated subforums counts */
 UPDATE wp_bbp_forums o LEFT JOIN (
     SELECT f.forum_id, count(p.ID) AS subforums
