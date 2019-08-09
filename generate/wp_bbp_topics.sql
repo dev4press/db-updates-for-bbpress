e0cc495976ab0aea423fdf4f0392f70c06264156
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-09 17:58:30 +0200

    Update wp_bbp_topics.sql

--- generate/wp_bbp_topics.sql
+++ generate/wp_bbp_topics.sql
@@ -1,12 +1,3 @@
-/* Insert initial topic/forum combinations */
-INSERT INTO wp_bbp_topics (`topic_id`, `forum_id`) ( 
-SELECT
-    t.ID AS topic_id,
-    f.ID AS forum_id
-FROM wp_posts t 
-INNER JOIN wp_posts f ON f.ID = t.post_parent AND f.post_type = 'forum' AND f.post_status IN ('publish', 'private', 'hidden')
-WHERE t.post_type = 'topic' AND t.post_status IN ('publish', 'closed', 'pending', 'trash', 'spam'));
-
 /* Update last_reply_id, last_active_id */
 UPDATE wp_bbp_topics t
 LEFT JOIN (
