89f8e7d082bea076ae45bdade6c3bec8a7750891
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-10 10:59:20 +0200

    Update wp_bbp_topics.sql

--- generate/updates/wp_bbp_topics.sql
+++ generate/updates/wp_bbp_topics.sql
@@ -1,17 +1,12 @@
 /* Update last_reply_id, last_active_id */
 UPDATE wp_bbp_topics t
-LEFT JOIN (
+INNER JOIN (
     SELECT t.topic_id, MAX(p.ID) AS last_reply_id
     FROM wp_bbp_topics t
     INNER JOIN wp_posts p ON p.post_parent = t.topic_id AND p.post_type = 'reply'
     WHERE p.post_status IN ('publish')
     GROUP BY t.topic_id) r ON t.topic_id = r.topic_id 
 SET t.last_reply_id = r.last_reply_id, t.last_active_id = r.last_reply_id;
-
-/* Update last_active_id if it is 0 */
-UPDATE wp_bbp_topics t
-SET t.last_active_id = t.topic_id
-WHERE t.last_active_id = 0;
 
 /* Update last_active_time */
 UPDATE wp_bbp_topics t
