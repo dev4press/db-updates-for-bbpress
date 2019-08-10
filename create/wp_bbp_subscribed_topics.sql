ef473f99f1f7b7a21be5bdaec900595576549a9f
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-10 16:52:02 +0200

    Update wp_bbp_subscribed_topics.sql

--- create/wp_bbp_subscribed_topics.sql
+++ create/wp_bbp_subscribed_topics.sql
@@ -1,7 +1,8 @@
+/* convert subscribed topics meta to own table */
 INSERT INTO wp_bbp_subscribed_topics
 SELECT 
-   CAST(pm.meta_value AS UNSIGNED) AS user_id, 
-   pm.post_id AS topic_id
+    CAST(pm.meta_value AS UNSIGNED) AS user_id, 
+    pm.post_id AS topic_id
 FROM wp_postmeta pm
 INNER JOIN wp_posts p ON p.ID = pm.post_id AND p.post_type = 'topic'
 WHERE pm.meta_key = '_bbp_subscription'
