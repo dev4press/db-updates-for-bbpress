c8acd3565803b40f8465205463be60c1ba17df47
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-10 15:40:49 +0200

    Create wp_bbp_forums_part_2.sql

---
+++ generate/updates/wp_bbp_forums_part_2.sql
@@ -0,0 +1,9 @@
+
+/* Update last_active_id */
+UPDATE wp_bbp_forums f 
+SET f.last_active_id = GREATEST(last_topic_id, last_reply_id);
+
+/* Update last_active_time */
+UPDATE wp_bbp_forums f
+INNER JOIN wp_posts p ON p.ID = f.last_active_id
+SET f.last_active_time = p.post_date;
