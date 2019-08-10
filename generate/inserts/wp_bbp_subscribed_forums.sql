f06cb8640266f1bdb9df3ff05cb5ae692abc47c1
Milan Petrovic <dev4press@users.noreply.github.com>
2019-08-10 16:51:30 +0200

    Update and rename create/wp_bbp_subscribed_forums.sql to generate/inserts/wp_bbp_subscribed_forums.sql

--- generate/inserts/wp_bbp_subscribed_forums.sql
+++ generate/inserts/wp_bbp_subscribed_forums.sql
@@ -1,7 +1,8 @@
+/* convert forum subscribed meta to own table */
 INSERT INTO wp_bbp_subscribed_forums
 SELECT 
-   CAST(pm.meta_value AS UNSIGNED) AS user_id, 
-   pm.post_id AS forum_id
+    CAST(pm.meta_value AS UNSIGNED) AS user_id, 
+    pm.post_id AS forum_id
 FROM wp_postmeta pm
 INNER JOIN wp_posts p ON p.ID = pm.post_id 
 AND p.post_type = 'forum'
