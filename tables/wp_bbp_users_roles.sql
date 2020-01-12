c2aa604e9b87fe55461fb3e1bda2c78dcd5db631
Milan Petrovic <dev4press@users.noreply.github.com>
2020-01-12 09:54:26 +0100

    Create wp_bbp_users_roles.sql

---
+++ tables/wp_bbp_users_roles.sql
@@ -0,0 +1,11 @@
+/* bbp_users_roles
+   this table connects users to specific forum roles
+*/
+
+CREATE TABLE `wp_bbp_users` (
+  `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
+  `role` VARCHAR(128) NOT NULL DEFAULT '',
+  UNIQUE INDEX `user_id` (`user_id`),
+  INDEX `role` (`role`)
+)
+ENGINE = InnoDB;
