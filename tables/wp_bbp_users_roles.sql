9491380e14da8c888d6bf608ac9cbd75c46b227d
Milan Petrovic <dev4press@users.noreply.github.com>
2021-01-02 20:51:37 +0100

    Update wp_bbp_users_roles.sql

--- tables/wp_bbp_users_roles.sql
+++ tables/wp_bbp_users_roles.sql
@@ -2,7 +2,7 @@
    this table connects users to specific forum roles
 */
 
-CREATE TABLE `wp_bbp_users` (
+CREATE TABLE `wp_bbp_users_roles` (
   `user_id` BIGINT UNSIGNED NOT NULL COMMENT 'Connects to ID in the wp_users table.',
   `role` VARCHAR(128) NOT NULL DEFAULT '',
   UNIQUE INDEX `user_id` (`user_id`),
